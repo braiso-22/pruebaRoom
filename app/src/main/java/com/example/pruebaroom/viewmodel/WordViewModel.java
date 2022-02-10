cbfdb57f3f87bfc38f3d33770fa3adca61408948
Brais <braisfv22@gmail.com>
2022-02-10 11:12:15 +0100

    Created ViewModel

---
+++ app/src/main/java/com/example/pruebaroom/viewmodel/WordViewModel.java
@@ -0,0 +1,31 @@
+package com.example.pruebaroom.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.pruebaroom.model.vo.Word;
+import com.example.pruebaroom.repository.WordRepository;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel {
+    private WordRepository mRepository;
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel(Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+    public void insert(Word word) {
+        mRepository.insert(word);
+    }
+}
